9db236c7c0cb05993f8c768317080f5e382fa12e
Tran Dai Thanh <daithanhtran2912@gmail.com>
2023-09-18 14:18:57 +0700

    completed TASK 1 & 2, update requirements.txt

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag housing_price_prediction_app:latest .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run --name "housing_price_prediction_app" -p 8000:80 housing_price_prediction_app:latest
