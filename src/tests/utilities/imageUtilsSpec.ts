e1f05939486147d7936579829239982d47a4f94d
Tran Dai Thanh <daithanhtran2912@gmail.com>
2023-10-08 01:04:23 +0700

    update prettierrc

--- src/tests/utilities/imageUtilsSpec.ts
+++ src/tests/utilities/imageUtilsSpec.ts
@@ -9,47 +9,53 @@ const height = 200;
 
 describe("Testing image valiation utility", (): void => {
   it("should throw error when filename is empty", async (): Promise<void> => {
-    await expectAsync(processImage("", height, width))
-      .toBeRejectedWithError("Unable to retrieve filename!");
+    await expectAsync(processImage("", height, width)).toBeRejectedWithError("Unable to retrieve filename!");
   });
 
   it("should throw error when filename does not exist", async (): Promise<void> => {
-    await expectAsync(processImage("test", height, width))
-      .toBeRejectedWithError("File name: test does not exist!");
+    await expectAsync(processImage("test", height, width)).toBeRejectedWithError(
+      "File name: test does not exist!"
+    );
   });
 
   it("should throw error when height is not a number", async (): Promise<void> => {
     const strHeight = "height" as unknown as number;
-    await expectAsync(processImage(filename, strHeight, width))
-      .toBeRejectedWithError("Either height is empty or not a number!");
+    await expectAsync(processImage(filename, strHeight, width)).toBeRejectedWithError(
+      "Either height is empty or not a number!"
+    );
   });
 
   it("should throw error when height is empty", async (): Promise<void> => {
     const strHeight = undefined as unknown as number;
-    await expectAsync(processImage(filename, strHeight, width))
-      .toBeRejectedWithError("Either height is empty or not a number!");
+    await expectAsync(processImage(filename, strHeight, width)).toBeRejectedWithError(
+      "Either height is empty or not a number!"
+    );
   });
 
   it("should throw error when height is smaller than 1", async (): Promise<void> => {
-    await expectAsync(processImage(filename, 0, width))
-      .toBeRejectedWithError("Height must be greater than 1!");
+    await expectAsync(processImage(filename, 0, width)).toBeRejectedWithError(
+      "Height must be greater than 1!"
+    );
   });
 
   it("should throw error when width is not a number", async (): Promise<void> => {
     const strWidth = "width" as unknown as number;
-    await expectAsync(processImage(filename, height, strWidth))
-      .toBeRejectedWithError("Either width is empty or not a number!");
+    await expectAsync(processImage(filename, height, strWidth)).toBeRejectedWithError(
+      "Either width is empty or not a number!"
+    );
   });
 
   it("should throw error when width is empty", async (): Promise<void> => {
     const strWidth = undefined as unknown as number;
-    await expectAsync(processImage(filename, height, strWidth))
-      .toBeRejectedWithError("Either width is empty or not a number!");
+    await expectAsync(processImage(filename, height, strWidth)).toBeRejectedWithError(
+      "Either width is empty or not a number!"
+    );
   });
 
   it("should throw error when width is smaller than 1", async (): Promise<void> => {
-    await expectAsync(processImage(filename, height, 0))
-      .toBeRejectedWithError("Width must be greater than 1!");
+    await expectAsync(processImage(filename, height, 0)).toBeRejectedWithError(
+      "Width must be greater than 1!"
+    );
   });
 });
 
