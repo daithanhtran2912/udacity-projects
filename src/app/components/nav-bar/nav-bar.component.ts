795def4cced4f6492c3a1a5543256ab1eb78249d
Tran Dai Thanh <daithanhtran2912@gmail.com>
2023-11-22 00:02:23 +0700

    add cart & product model, update footer & navbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faCartShopping, faArrowRightToBracket, faListUl } from '@fortawesome/free-solid-svg-icons';
+import { CartService } from '../../services/cart.service';
+import { Cart } from '../../models/cart';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   faCartShopping = faCartShopping;
   faArrowRightToBracket = faArrowRightToBracket;
   faListUl = faListUl;
+  @Input() cart!: Cart;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cart = this.cartService.getCart();
+  }
 }
